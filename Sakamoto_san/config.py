de51b09f0dc1afdc3bc3f3eb8e7c54264f27d980
Iury da Fonsêca Silva <iury.silva@aluno.ufop.edu.br>
2020-09-29 22:03:24 -0300

    Impementei os metodos
    - para a inicializa;áo da assistente
    - para a o tratamento do nome do usuario, bem como sua captura na classe main

--- Sakamoto_san/config.py
+++ Sakamoto_san/config.py
@@ -1,3 +1,9 @@
+version = "1.4.0"
+
+def intro():
+    mensagem = "Assistente - version {} / by: Fulano Beltrano Ciclano".format(version)
+    print("-" * len(mensagem) + "\n{}\n".format(mensagem) + "-" * len(mensagem))
+
 listaErros = [
     "Não entendi....",
     "Desculpe, não entendi",
@@ -13,3 +19,55 @@ comandos = {
     "desligar": "desligando",
     "reiniciar": "reiniciando"
 }
+
+def verificaNome(userName):
+    if userName.startswith("Meu nome é"):
+        userName = userName.replace("Meu nome é", "")
+    if userName.startswith("Eu me chamo"):
+        userName = userName.replace("Eu me chamo", "")
+    if userName.startswith("Eu sou o"):
+        userName = userName.replace("Eu sou o", "")
+    if userName.startswith("Eu sou a"):
+        userName = userName.replace("Eu sou a", "")
+    
+    return userName
+
+def verificaNomeExiste(nome):
+    dados = open("dados/nomes.txt", "r")
+    nomeList = dados.readlines()
+    
+    if not nomeList:
+        vaxio = open("dados/nomes.txt", "r")#abre o arquivo nomes no diretorio dados no modo de leitura
+        conteudo = vazio.readlines()
+        conteudo.append("{}".format(nome))
+        vazio = open("dados/nomes.txt", "w")#abre o arquivo nomes no diretorio dados no modo de escrita
+        vazio.writelines(conteudo)
+        vazio.close()
+        
+        return "Hello {}, nice to meet you!".format(nome)
+    
+    for linha in nomeList:
+        if linha == nome:
+            return "Hello {}, we haven been meeted!".format(nome)
+    
+    #abre o arquivo nomes no diretorio dados no modo de leitura    
+    vaxio = open("dados/nomes.txt", "r")
+    conteudo = vazio.readlines()
+    conteudo.append("\n{}".format(nome))
+    #abre o arquivo nomes no diretorio dados no modo de escrita
+    vazio = open("dados/nomes.txt", "w")
+    vazio.writelines(conteudo)
+    vazio.close()
+    
+    return "Hello {}, it's the first time than we talking".format(nome)
+
+def nameList():
+    try: 
+        nomes = open("dados/nomes.txt", "r")
+        nomes.close()
+    
+    # Cria a lista de nomes caso ela não seja encontrada
+    except FileNotFoundError:
+        nomes = open("dados/nomes.txt", "w")
+        nomes.close()
+    
--- Sakamoto_san/main.py
+++ Sakamoto_san/main.py
@@ -9,24 +9,61 @@ def saidaSom(resposta):
     reproducao.say(resposta)
     reproducao.runAndWait()
 
-print("Ouvindo.......\n----------------\n")
-while True:
-    respostaErroAleatoria = choice(listaErros)
-    reconhecimento = reconhecimentoDeFala.Recognizer()
+def assistente():
+    print("Hello, what's you full name?") 
+    saidaSom("Hello, what's you full name?")
+    
+    while True:
+        respostaErroAleatoria = choice(listaErros)
+        reconhecimento = reconhecimentoDeFala.Recognizer()
 
-    with reconhecimentoDeFala.Microphone() as microfone:
-        reconhecimento.adjust_for_ambient_noise(microfone)
+        with reconhecimentoDeFala.Microphone() as microfone:
+            reconhecimento.adjust_for_ambient_noise(microfone)
 
-        while True:
-            try:
-                audio = reconhecimento.listen(microfone)
-                entrada = reconhecimento.recognize_google(audio, language='en')
-                print("Você disse: {}".format(entrada))
-                resposta = conversas[entrada]    
-                print("Sakamoto-san: {}".format(resposta))
-                saidaSom("{}".format(resposta))
-                         
-            except reconhecimentoDeFala.UnknownValueError:
-                saidaSom(respostaErroAleatoria)
+            while True:
+                try:
+                    audio = reconhecimento.listen(microfone)
+                    userName = reconhecimento.recognize_google(audio, language='en')
+                    userName = verificaNome(userName)
+                    nameList()
+                    apresentacao = "{}".format(verificaNomeExiste(userName))
+                    print(apresentacao)
+                    saidaSom(apresentacao)
+                    
+                    # Cria uma lista com cada parte de um nome separada por " "
+                    bruteUserName = userName
+                    userName = userName.split(" ")
+                    userName = userName[0]
+                    break
+                            
+                except reconhecimentoDeFala.UnknownValueError:
+                    saidaSom(respostaErroAleatoria)
+        break
+    
+    print("=" * len(apresentacao))
+    print("Ouvindo...")
+    
+    while True:
+        respostaErroAleatoria = choice(listaErros)
+        reconhecimento = reconhecimentoDeFala.Recognizer()
+
+        with reconhecimentoDeFala.Microphone() as microfone:
+            reconhecimento.adjust_for_ambient_noise(microfone)
+
+            while True:
+                try:
+                    audio = reconhecimento.listen(microfone)
+                    entrada = reconhecimento.recognize_google(audio, language='en')
+                    print("{}: {}".format(userName, entrada))
+                    resposta = conversas[entrada]    
+                    print("Sakamoto-san: {}".format(resposta))
+                    saidaSom("{}".format(resposta))
+                            
+                except reconhecimentoDeFala.UnknownValueError:
+                    saidaSom(respostaErroAleatoria)
             
-
+if __name__ == '__main__':
+    intro()
+    saidaSom("Iniciando")
+    assistente()
+    
